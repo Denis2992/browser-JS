cd138b6d4b7a5d3e03d60974dfeb310ca23d5fc0
Denis Dadalski <denis.dadalski@gmail.com>
2021-12-04 11:24:09 +0100

    ready task

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 const btn = document.querySelector('.btn-primary');
+const erase = document.querySelector('.erase');
 const evenList = document.querySelector('.even_list');
 const oddList = document.querySelector('.odd_list');
 
-let numbersArr = [];
-let evenArr = [];
-let oddArr = [];
+
 
 function renderNumbers (arr, selector) {
-    arr.sort((a, b) => a - b).forEach(el => {
-        const newLi = document.createElement('li');
-        newLi.innerText = el;
-        newLi.style.listStyle = 'none';
-        selector.append(newLi);
-    });
+   selector.innerText = arr.sort((a, b) => a - b).join('\n');
+
 }
 
 btn.addEventListener('click', () => {
+    let numbersArr = [];
+    let evenArr = [];
+    let oddArr = [];
     const min = 1;
     const max = 100;
 
     for (let i=0; i<20; i++) {
-        numbersArr.push(Math.floor(Math.random() * (max - min + 1)) + min)
+        numbersArr.push(Math.floor(Math.random() * (max - min + 1)) + min);
     }
 
     numbersArr.forEach(el => {
@@ -33,4 +31,6 @@ btn.addEventListener('click', () => {
 
     renderNumbers(evenArr, evenList);
     renderNumbers(oddArr, oddList);
-})
+    
+});
+
